be1eb24b6b4526b2c8f697d501d333b7a51d0123
Anagha Kelkar <aukelkar@gmail.com>
2018-02-17 02:43:14 -0800

    devise login completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
-  devise_for :users
-  resources :users
-  resources :pages do
-    collection do
-      get :about
-    end
-    end
+	root to: 'visitors#index'
+	devise_for :users
+	resources :users
+	resources :pages do
+  	collection do
+    	get :about
+  	end
+ 	end
 end
